c26692b7345083240184606cb009c2cb545a3532
basam <basam.najwa11@gmail.com>
2026-01-05 19:26:18 +0400

    added the contact links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,8 @@ const Contact = () => {
     <section className="section contact">
       <h3 className="section-title">Contacts</h3>
       <a href="mailto:basambasam@hotmail.com">Email</a>
-      <a href="https://github.com/">GitHub</a>
-      <a href="https://linkedin.com/basabasam">LinkedIn</a>
+      <a href="https://github.com/BasamLive">GitHub</a>
+      <a href="https://linkedin.com/in/basam249">LinkedIn</a>
       <a href="tel:+971557657992">+971- 557657992</a>
     </section>
   );
